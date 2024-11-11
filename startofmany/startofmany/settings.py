5063a07ce501a76470e67d9b130601a24affdcd3
Danielngemi <dantezmalusi254@gmail.com>
2024-11-11 14:36:18 +0300

    Updated database settings to use SQLite3

--- startofmany/startofmany/settings.py
+++ startofmany/startofmany/settings.py
@@ -98,8 +98,10 @@ WSGI_APPLICATION = 'startofmany.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default':
-        dj_database_url.config(default=config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Session Configuration
